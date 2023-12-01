f5dcc3120e9001aa7d0633a47aa0f59edc283a91
Argyrios Tsagkalidis <arg.tsag@gmail.com>
2023-12-01 21:24:58 +0200

    Fix: replaced match-cace with if-elif-else conditionals
    Now the package can work with older Python versions. Before the fix, the module needed at least Python 3.10 to work, due to usage of match keyword.

--- src/fictional_names/name_generator.py
+++ src/fictional_names/name_generator.py
@@ -1,3 +1,4 @@
+import sys
 from random import choice
 from .supportive_functions import (
     generate_female_name,
@@ -19,115 +20,113 @@ def generate_name(gender=choice(["female", "male"]), style=choice(styles), libra
     
     # Female names section
     if gender.lower() == "female":
-        match style:
-            case "elven":
-                name = choice(elven.female_names) if library else generate_female_name(elven.female_prefix, elven.female_suffix)
-            case "dwarven":
-                name = choice(dwarven.female_names) if library else generate_female_name(dwarven.female_prefix, dwarven.female_suffix, min=1, max=2)
-            case "giant":
-                name = choice(giant.female_names) if library else generate_female_name(giant.female_prefix, giant.female_suffix, min=0, max=1)
-            case "halfling":
-                name = choice(halfling.female_names) if library else generate_female_name(halfling.female_prefix, halfling.female_suffix)
-            case "orc":
-                name = choice(orc.female_names) if library else generate_female_name(orc.female_prefix, orc.female_suffix, min=0, max=1)
-            case "steampunk":
-                name = choice(steampunk.female_names_lbr) if library else choice(steampunk.female_names)
-            case "human":
-                name = choice(human.female_names)
-            case "english":
-                name = choice(english.female_names_lbr) if library else choice(english.female_names)
-            case "greek":
-                name = choice(greek.female_names) if library else generate_female_name(greek.female_prefix, greek.female_suffix)
-            case "norsemen":
-                name = choice(norsemen.female_names) if library else generate_female_name(norsemen.female_prefix, norsemen.female_suffix, min=0, max=1)
-            case "arab":
-                name = choice(arab.female_names) if library else generate_female_name(arab.female_prefix, arab.female_suffix, min=1, max=2)
-            case "japanese":
-                name = choice(japanese.female_names) if library else generate_female_name(japanese.female_prefix, japanese.female_suffix)
-            case "viking":
-                name = choice(viking.female_names) if library else generate_female_name(viking.female_prefix, viking.female_suffix)
-            case "germanic":
-                name = choice(germanic.female_names) if library else generate_female_name(germanic.female_prefix, germanic.female_suffix)
-            case "turkish":
-                name = choice(turkish.female_names) if library else generate_female_name(turkish.female_prefix, turkish.female_suffix)
-            case "chinese":
-                name = choice(chinese.female_names) if library else generate_female_name(chinese.female_prefix, chinese.female_suffix)
-            case "mongolian":
-                name = choice(mongolian.female_names) if library else generate_female_name(mongolian.female_prefix, mongolian.female_suffix)
-            case "slavic":
-                name = choice(slavic.female_names) if library else generate_female_name(slavic.female_prefix, slavic.female_suffix)
-            case "aztec":
-                name = choice(aztec.female_names) if library else generate_female_name(aztec.female_prefix, aztec.female_suffix)
-            case "roman":
-                name = choice(roman.female_names) if library else generate_female_name(roman.female_prefix, roman.female_suffix)
-            case "tolkien":
-                name = choice(tolkien.female_names) if library else generate_female_name(tolkien.female_prefix, tolkien.female_suffix, min=1, max=2)
-            case "martin":
-                name = choice(martin.female_names) if library else generate_female_name(martin.female_prefix, martin.female_suffix, min=0, max=1)
-            case "jordan":
-                name = choice(jordan.female_names) if library else generate_female_name(jordan.female_prefix, jordan.female_suffix, min=0, max=2)
-            case "erikson":
-                name = choice(erikson.female_names) if library else generate_female_name(erikson.female_prefix, jordan.female_suffix, min=0, max=2)
-            case "rowling":
-                name = choice(rowling.female_names) if library else generate_female_name(rowling.female_prefix, rowling.female_suffix)
-            case "sapkowski":
-                name = choice(sapkowski.female_names) if library else generate_female_name(sapkowski.female_prefix, sapkowski.female_suffix)
+        if style.lower() == "elven":
+            name = choice(elven.female_names) if library else generate_female_name(elven.female_prefix, elven.female_suffix)
+        elif style.lower() == "dwarven":
+            name = choice(dwarven.female_names) if library else generate_female_name(dwarven.female_prefix, dwarven.female_suffix, min=1, max=2)
+        elif style.lower() == "giant":
+            name = choice(giant.female_names) if library else generate_female_name(giant.female_prefix, giant.female_suffix, min=0, max=1)
+        elif style.lower() == "halfling":
+            name = choice(halfling.female_names) if library else generate_female_name(halfling.female_prefix, halfling.female_suffix)
+        elif style.lower() == "orc":
+            name = choice(orc.female_names) if library else generate_female_name(orc.female_prefix, orc.female_suffix, min=0, max=1)
+        elif style.lower() == "steampunk":
+            name = choice(steampunk.female_names_lbr) if library else choice(steampunk.female_names)
+        elif style.lower() == "human":
+            name = choice(human.female_names)
+        elif style.lower() == "english":
+            name = choice(english.female_names_lbr) if library else choice(english.female_names)
+        elif style.lower() == "greek":
+            name = choice(greek.female_names) if library else generate_female_name(greek.female_prefix, greek.female_suffix)
+        elif style.lower() == "norsemen":
+            name = choice(norsemen.female_names) if library else generate_female_name(norsemen.female_prefix, norsemen.female_suffix, min=0, max=1)
+        elif style.lower() == "arab":
+            name = choice(arab.female_names) if library else generate_female_name(arab.female_prefix, arab.female_suffix, min=1, max=2)
+        elif style.lower() == "japanese":
+            name = choice(japanese.female_names) if library else generate_female_name(japanese.female_prefix, japanese.female_suffix)
+        elif style.lower() == "viking":
+            name = choice(viking.female_names) if library else generate_female_name(viking.female_prefix, viking.female_suffix)
+        elif style.lower() == "germanic":
+            name = choice(germanic.female_names) if library else generate_female_name(germanic.female_prefix, germanic.female_suffix)
+        elif style.lower() == "turkish":
+            name = choice(turkish.female_names) if library else generate_female_name(turkish.female_prefix, turkish.female_suffix)
+        elif style.lower() == "chinese":
+            name = choice(chinese.female_names) if library else generate_female_name(chinese.female_prefix, chinese.female_suffix)
+        elif style.lower() == "mongolian":
+            name = choice(mongolian.female_names) if library else generate_female_name(mongolian.female_prefix, mongolian.female_suffix)
+        elif style.lower() == "slavic":
+            name = choice(slavic.female_names) if library else generate_female_name(slavic.female_prefix, slavic.female_suffix)
+        elif style.lower() == "aztec":
+            name = choice(aztec.female_names) if library else generate_female_name(aztec.female_prefix, aztec.female_suffix)
+        elif style.lower() == "roman":
+            name = choice(roman.female_names) if library else generate_female_name(roman.female_prefix, roman.female_suffix)
+        elif style.lower() == "tolkien":
+            name = choice(tolkien.female_names) if library else generate_female_name(tolkien.female_prefix, tolkien.female_suffix, min=1, max=2)
+        elif style.lower() == "martin":
+            name = choice(martin.female_names) if library else generate_female_name(martin.female_prefix, martin.female_suffix, min=0, max=1)
+        elif style.lower() == "jordan":
+            name = choice(jordan.female_names) if library else generate_female_name(jordan.female_prefix, jordan.female_suffix, min=0, max=2)
+        elif style.lower() == "erikson":
+            name = choice(erikson.female_names) if library else generate_female_name(erikson.female_prefix, jordan.female_suffix, min=0, max=2)
+        elif style.lower() == "rowling":
+            name = choice(rowling.female_names) if library else generate_female_name(rowling.female_prefix, rowling.female_suffix)
+        elif style.lower() == "sapkowski":
+            name = choice(sapkowski.female_names) if library else generate_female_name(sapkowski.female_prefix, sapkowski.female_suffix)
 
     # Male names section
     elif gender.lower() == "male":
-        match style:
-            case "elven":
-                name = choice(elven.male_names) if library else generate_male_name(elven.male_prefix, elven.male_suffix)
-            case "dwarven":
-                name = choice(dwarven.male_names) if library else generate_male_name(dwarven.male_prefix, dwarven.male_suffix)
-            case "giant":
-                name = choice(giant.male_names) if library else generate_male_name(giant.male_prefix, giant.male_suffix, min=0, max=1)
-            case "halfling":
-                name = choice(halfling.male_names) if library else generate_male_name(halfling.male_prefix, halfling.male_suffix)
-            case "orc":
-                name = choice(orc.male_names) if library else generate_male_name(orc.male_prefix, orc.male_suffix, min=0, max=1)
-            case "steampunk":
-                name = choice(steampunk.male_names_lbr) if library else choice(steampunk.male_names)
-            case "human":
-                name = choice(human.male_names)
-            case "english":
-                name = choice(english.male_names_lbr) if library else choice(english.male_names)
-            case "greek":
-                name = choice(greek.male_names) if library else generate_male_name(greek.male_prefix, greek.male_suffix)
-            case "norsemen":
-                name = choice(norsemen.male_names) if library else generate_male_name(norsemen.male_prefix, norsemen.male_suffix, min=0, max=1)
-            case "arab":
-                name = choice(arab.male_names) if library else generate_male_name(arab.male_prefix, arab.male_suffix, min=1, max=2)
-            case "japanese":
-                name = choice(japanese.male_names) if library else generate_male_name(japanese.male_prefix, japanese.male_suffix)
-            case "viking":
-                name = choice(viking.male_names) if library else generate_male_name(viking.male_prefix, viking.male_suffix)
-            case "germanic":
-                name = choice(germanic.male_names) if library else generate_male_name(germanic.male_prefix, germanic.male_suffix)
-            case "turkish":
-                name = choice(turkish.male_names) if library else generate_male_name(turkish.male_prefix, turkish.male_suffix)
-            case "chinese":
-                name = choice(chinese.male_names) if library else generate_male_name(chinese.male_prefix, chinese.male_suffix)
-            case "mongolian":
-                name = choice(mongolian.male_names) if library else generate_male_name(mongolian.male_prefix, mongolian.male_suffix)
-            case "slavic":
-                name = choice(slavic.male_names) if library else generate_male_name(slavic.male_prefix, slavic.male_suffix)
-            case "aztec":
-                name = choice(aztec.male_names) if library else generate_male_name(aztec.male_prefix, aztec.male_suffix)
-            case "roman":
-                name = choice(roman.male_names) if library else generate_male_name(roman.male_prefix, roman.male_suffix)
-            case "tolkien":
-                name = choice(tolkien.male_names) if library else generate_male_name(tolkien.male_prefix, tolkien.male_suffix, min=1, max=2)
-            case "martin":
-                name = choice(martin.male_names) if library else generate_male_name(martin.male_prefix, martin.male_suffix, min=0, max=1)
-            case "jordan":
-                name = choice(jordan.male_names) if library else generate_male_name(jordan.male_prefix, jordan.male_suffix, min=0, max=2)
-            case "erikson":
-                name = choice(erikson.male_names) if library else generate_male_name(erikson.male_prefix, erikson.male_suffix, min=0, max=2)
-            case "rowling":
-                name = choice(rowling.male_names) if library else generate_male_name(rowling.male_prefix, rowling.male_suffix)
-            case "sapkowski":
-                name = choice(sapkowski.male_names) if library else generate_male_name(sapkowski.male_prefix, sapkowski.male_suffix)
+        if style.lower() == "elven":
+            name = choice(elven.male_names) if library else generate_male_name(elven.male_prefix, elven.male_suffix)
+        elif style.lower() == "dwarven":
+            name = choice(dwarven.male_names) if library else generate_male_name(dwarven.male_prefix, dwarven.male_suffix)
+        elif style.lower() == "giant":
+            name = choice(giant.male_names) if library else generate_male_name(giant.male_prefix, giant.male_suffix, min=0, max=1)
+        elif style.lower() == "halfling":
+            name = choice(halfling.male_names) if library else generate_male_name(halfling.male_prefix, halfling.male_suffix)
+        elif style.lower() == "orc":
+            name = choice(orc.male_names) if library else generate_male_name(orc.male_prefix, orc.male_suffix, min=0, max=1)
+        elif style.lower() == "steampunk":
+            name = choice(steampunk.male_names_lbr) if library else choice(steampunk.male_names)
+        elif style.lower() == "human":
+            name = choice(human.male_names)
+        elif style.lower() == "english":
+            name = choice(english.male_names_lbr) if library else choice(english.male_names)
+        elif style.lower() == "greek":
+            name = choice(greek.male_names) if library else generate_male_name(greek.male_prefix, greek.male_suffix)
+        elif style.lower() == "norsemen":
+            name = choice(norsemen.male_names) if library else generate_male_name(norsemen.male_prefix, norsemen.male_suffix, min=0, max=1)
+        elif style.lower() == "arab":
+            name = choice(arab.male_names) if library else generate_male_name(arab.male_prefix, arab.male_suffix, min=1, max=2)
+        elif style.lower() == "japanese":
+            name = choice(japanese.male_names) if library else generate_male_name(japanese.male_prefix, japanese.male_suffix)
+        elif style.lower() == "viking":
+            name = choice(viking.male_names) if library else generate_male_name(viking.male_prefix, viking.male_suffix)
+        elif style.lower() == "germanic":
+            name = choice(germanic.male_names) if library else generate_male_name(germanic.male_prefix, germanic.male_suffix)
+        elif style.lower() == "turkish":
+            name = choice(turkish.male_names) if library else generate_male_name(turkish.male_prefix, turkish.male_suffix)
+        elif style.lower() == "chinese":
+            name = choice(chinese.male_names) if library else generate_male_name(chinese.male_prefix, chinese.male_suffix)
+        elif style.lower() == "mongolian":
+            name = choice(mongolian.male_names) if library else generate_male_name(mongolian.male_prefix, mongolian.male_suffix)
+        elif style.lower() == "slavic":
+            name = choice(slavic.male_names) if library else generate_male_name(slavic.male_prefix, slavic.male_suffix)
+        elif style.lower() == "aztec":
+            name = choice(aztec.male_names) if library else generate_male_name(aztec.male_prefix, aztec.male_suffix)
+        elif style.lower() == "roman":
+            name = choice(roman.male_names) if library else generate_male_name(roman.male_prefix, roman.male_suffix)
+        elif style.lower() == "tolkien":
+            name = choice(tolkien.male_names) if library else generate_male_name(tolkien.male_prefix, tolkien.male_suffix, min=1, max=2)
+        elif style.lower() == "martin":
+            name = choice(martin.male_names) if library else generate_male_name(martin.male_prefix, martin.male_suffix, min=0, max=1)
+        elif style.lower() == "jordan":
+            name = choice(jordan.male_names) if library else generate_male_name(jordan.male_prefix, jordan.male_suffix, min=0, max=2)
+        elif style.lower() == "erikson":
+            name = choice(erikson.male_names) if library else generate_male_name(erikson.male_prefix, erikson.male_suffix, min=0, max=2)
+        elif style.lower() == "rowling":
+            name = choice(rowling.male_names) if library else generate_male_name(rowling.male_prefix, rowling.male_suffix)
+        elif style.lower() == "sapkowski":
+            name = choice(sapkowski.male_names) if library else generate_male_name(sapkowski.male_prefix, sapkowski.male_suffix)
     else:
         raise ValueError("Invalid gender specified. Please choose 'male' or 'female'.")
 
@@ -193,5 +192,8 @@ def generate_name(gender=choice(["female", "male"]), style=choice(styles), libra
 
 
 if __name__ == "__main__":
-    generated_name = generate_name()
+    if len(sys.argv) > 1:
+        generated_name = generate_name(gender=sys.argv[1], style=sys.argv[2], library=bool(sys.argv[3]))
+    else:
+        generated_name = generate_name()
     print(generated_name)
